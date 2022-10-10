75aa20e0bee11d4950556ab0e1e891dd4b31cffa
iamalexneumann <iamalexneumann@yandex.ru>
2022-10-10 20:33:11 +0300

    Добавляет шаблон списка сообщений

---
+++ local/templates/tf_shop/components/bitrix/news.list/message_list/lang/ru/template.php
@@ -0,0 +1,6 @@
+<?php
+$MESS['CT_BNL_ELEMENT_DELETE_CONFIRM'] = 'Будет удалена вся информация, связанная с этой записью. Продолжить?';
+$MESS['MESSAGE_LIST_BTN_TEXT'] = 'Подробнее';
+$MESS['MESSAGE_LIST_CAROUSEL_PREV_BTN_TEXT'] = 'Предыдущее сообщение';
+$MESS['MESSAGE_LIST_CAROUSEL_NEXT_BTN_TEXT'] = 'Следующее сообщение';
+$MESS['MESSAGE_LIST_BTN_CLOSE_LABEL'] = 'Закрыть';
--- local/templates/tf_shop/header.php
+++ local/templates/tf_shop/header.php
@@ -41,6 +41,66 @@ Loc::loadLanguageFile(__FILE__);
 <body>
 <?= $siteparam_scripts_body_before; ?>
 <?php $APPLICATION->ShowPanel(); ?>
+<?php
+$APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "message_list",
+    Array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "COMPOSITE_FRAME_MODE" => "A",
+        "COMPOSITE_FRAME_TYPE" => "AUTO",
+        "DETAIL_URL" => "",
+        "DISPLAY_BOTTOM_PAGER" => "N",
+        "DISPLAY_DATE" => "N",
+        "DISPLAY_NAME" => "N",
+        "DISPLAY_PICTURE" => "N",
+        "DISPLAY_PREVIEW_TEXT" => "N",
+        "DISPLAY_TOP_PAGER" => "N",
+        "FIELD_CODE" => array(0=>"",1=>"",),
+        "FILTER_NAME" => "",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => "4",
+        "IBLOCK_TYPE" => "secondary_content",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "INCLUDE_SUBSECTIONS" => "Y",
+        "MESSAGE_404" => "",
+        "NEWS_COUNT" => "5",
+        "PAGER_BASE_LINK_ENABLE" => "N",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "",
+        "PARENT_SECTION" => "",
+        "PARENT_SECTION_CODE" => "",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "PROPERTY_CODE" => array(0=>"ATT_BTN_SHOW",1=>"ATT_BTN_LINK",2=>"ATT_BTN_TEXT",3=>"",),
+        "SET_BROWSER_TITLE" => "N",
+        "SET_LAST_MODIFIED" => "N",
+        "SET_META_DESCRIPTION" => "N",
+        "SET_META_KEYWORDS" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SHOW_404" => "N",
+        "SORT_BY1" => "SORT",
+        "SORT_BY2" => "ACTIVE_FROM",
+        "SORT_ORDER1" => "ASC",
+        "SORT_ORDER2" => "DESC",
+        "STRICT_SECTION_CHECK" => "Y"
+    )
+); ?>
 <header class="main-header sticky-top">
     <nav class="navbar navbar-expand-xl bg-white">
         <div class="container-fluid">
